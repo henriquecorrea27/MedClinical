76ea79877fc7a5089b1dc6ff3d28c0333c72e0cf
henriquecorrea27 <110430931+henriquecorrea27@users.noreply.github.com>
2023-12-01 16:11:44 -0300

    Add files via upload

---
+++ funcoes.js
@@ -0,0 +1,59 @@
+function sair() {
+  var sair = "index.html";
+
+  window.location.href = sair;
+}
+
+function prontuario() {
+  var prontuario = "medicalHistory.html";
+
+  window.location.href = prontuario;
+}
+
+function voltarTelaDps() {
+  var voltar = "telaDpsLogin.html";
+
+  window.location.href = voltar;
+}
+
+function novoProntuario() {
+  var novoProntuario = "novoProntuario.html";
+
+  window.location.href = novoProntuario;
+}
+
+function agenda() {
+  var agenda = "agenda.html";
+
+  window.location.href = agenda;
+}
+
+function receitas() {
+  var receita = "receitas.html";
+
+  window.location.href = receita;
+}
+
+function atestados() {
+  var atestado = "atestados.html";
+
+  window.location.href = atestado;
+}
+
+function remedios() {
+  var remedio = "remedios.html";
+
+  window.location.href = remedio;
+}
+
+function perfil() {
+  var perfil = "perfil.html";
+
+  window.location.href = perfil;
+}
+
+function chat() {
+  var chat = "chat.html";
+
+  window.location.href = chat;
+}
